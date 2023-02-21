8ed431c8418704f44113c09cfdc7a861c6211aa7
anugarg83in <44963586+anugarg83in@users.noreply.github.com>
2023-02-21 23:33:33 +0530

    Add files via upload

---
+++ Global.js
@@ -0,0 +1,17 @@
+const listedFeedURLs = [
+  "https://dev.to/feed/",
+  "https://devblogs.microsoft.com/dotnet/category/aspnetcore/feed/",
+  "https://www.interviewbit.com/blog/feed/",
+  "https://blog.jetbrains.com/feed/",
+];
+const listedUrls = [
+  { Name: "dev", Url: "https://dev.to/feed/" },
+  {
+    Name: "devblogs",
+    Url: "https://devblogs.microsoft.com/dotnet/category/aspnetcore/feed/",
+  },
+  { Name: "interviewbit", Url: "https://www.interviewbit.com/blog/feed/" },
+  { Name: "jetbrains", Url: "https://blog.jetbrains.com/feed/" },
+];
+const filterFeeds =
+  ".Net|Docker|ASP.NET|Node.js|dotnet|core|improvement|performance|system design|javascript|C#|List|LINQ|Queue|Network";
